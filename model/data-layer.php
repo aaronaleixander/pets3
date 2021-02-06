ba3ac9ee6ac0bdd2921f54279c7494569dbaa4bc
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-02-06 17:40:59 -0500

    addedsize and accessory functions to data layer

--- model/data-layer.php
+++ model/data-layer.php
@@ -7,3 +7,21 @@ function getColors(){
     $colors = array("red", "green", "blue");
     return $colors;
 }
+
+/*
+ * Return a list of pet sizes
+ * @return array
+ */
+function getSizes(){
+    $sizes = array("small", "medium", "large");
+    return $sizes;
+}
+
+/*
+ * Return a list of pet accessories
+ * @return array
+ */
+function getAccessories(){
+    $accs = array("bow", "shoes", "rain jacket", "sweater");
+    return $accs;
+}
