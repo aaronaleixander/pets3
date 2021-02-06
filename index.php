3052651b7515fe042f112704a02ae5c9677d187f
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-02-06 01:01:24 -0500

    added model directory and updated require() for data-layer.php

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ error_reporting(E_ALL);
 session_start();
 //Require the autoload file
 require_once('vendor/autoload.php');
+require_once('model/data-layer.php');
 
 //Create an instance of the Base class
 $f3 = Base::instance();
---
+++ model/data-layer.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Return a list of colors
+ * @return array
+ */
+function getColors(){
+    $colors = array("red", "green", "blue");
+    return colors;
+}
